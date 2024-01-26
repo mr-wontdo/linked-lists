5c728f2b2d1ad1bdb423aa6cce977c546900d5f4
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2024-01-25 23:55:33 -0800

    Add append function

--- linked-list.js
+++ linked-list.js
@@ -4,4 +4,16 @@ export default class LinkedList {
     constructor(headNode = null) {
         this.headNode = headNode;
     }
+
+    append(value) {
+        if (this.headNode === null) {
+            this.headNode = new Node(value);
+        } else {
+            let pointer = this.headNode;
+            while (pointer.nextNode) {
+                pointer = pointer.nextNode;
+            }
+            pointer.nextNode = new Node(value);
+        }
+    }
 }
