1ca9a4af181daf96728c0945fb78f17a5d1eb482
mr-wontdo <122188740+mr-wontdo@users.noreply.github.com>
2024-01-27 11:56:03 -0800

    Add size function

--- linked-list.js
+++ linked-list.js
@@ -22,4 +22,14 @@ export default class LinkedList {
         this.headNode = new Node(value);
         this.headNode.nextNode = pointer;
     }
+
+    size() {
+        let count = 0;
+        let pointer = this.headNode;
+        while (pointer) {
+            count++;
+            pointer = pointer.nextNode;
+        }
+        return count;
+    }
 }
